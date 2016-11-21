0c77a58bfc02b46f5c24ad3f5efe4cbc2e21a480
Chris Woodside <christopherwoodside@gmail.com>
2016-11-21 13:29:51 -0600

    Added Edit and Update methods and views.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -20,6 +20,32 @@ class ClientsController < ApplicationController
   end
 
   def show
-    
+    client_id = params[:id]
+    @client = Client.find_by(id: client_id)
+    render 'show.html.erb'
+  end
+
+  def edit
+    client_id = params[:id]
+    @client = Client.find_by(id: client_id)
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_id = params[:id]
+    @client = Client.find_by(id: client_id)
+    client.first_name = params[:first_name]
+    client.last_name = params[:last_name]
+    client.injury = params[:injury]
+    client.appointment_date = params[:appointment_date]
+    client.save
+    redirect_to '/clients/#{client.id}'
+  end
+
+  def destroy
+    client_id = params[:id]
+    @client = Client.find_by(id: client_id)
+    client.destroy
+    redirect_to '/clients'
   end
 end
